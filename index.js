0715c4afae3b7e6e1a115805a2541a6485e6a3cf
paulosradavelli <paulosradavelli@gmail.com>
2023-01-05 18:23:13 -0300

    Style buttons, style footer, finish project

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ const r = document.querySelector(':root');
 const sizeSelector = document.querySelector(".size-selector");
 const value = document.querySelector(".value");
 let color = document.querySelector(".color");
+const button = document.querySelector(".btn");
 
 function createTable(gridSize) {
   r.style.setProperty('--size', gridSize)
   container.innerHTML= "";
   for (let i = 0; i < gridSize*gridSize; i += 1) {
     const createDiv = document.createElement('div');
+    
     createDiv.addEventListener('mouseleave', () => {
       createDiv.style = `background-color: ${r.style.getPropertyValue('--colorPick')};`
-    })
+    });
+
+    button.addEventListener('click',()=>{
+      createDiv.style= "background-color: #ffffff";
+    });
+
     container.appendChild(createDiv);
   }
 }
+
 
 function changeSize(gridSize){
   createTable(gridSize)
