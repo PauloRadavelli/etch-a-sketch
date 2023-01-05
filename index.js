76abc7843d096657b4f08bf5bda82a794f65043e
paulosradavelli <paulosradavelli@gmail.com>
2023-01-05 17:56:43 -0300

    Add color picker

---
+++ index.js
@@ -0,0 +1,34 @@
+const container = document.querySelector(".wrapper");
+const r = document.querySelector(':root');
+const sizeSelector = document.querySelector(".size-selector");
+const value = document.querySelector(".value");
+let color = document.querySelector(".color");
+
+function createTable(gridSize) {
+  r.style.setProperty('--size', gridSize)
+  container.innerHTML= "";
+  for (let i = 0; i < gridSize*gridSize; i += 1) {
+    const createDiv = document.createElement('div');
+    createDiv.addEventListener('mouseleave', () => {
+      createDiv.style = `background-color: ${r.style.getPropertyValue('--colorPick')};`
+    })
+    container.appendChild(createDiv);
+  }
+}
+
+function changeSize(gridSize){
+  createTable(gridSize)
+}
+
+sizeSelector.addEventListener("input", ()=>{
+    changeSize(sizeSelector.value);
+    value.textContent = sizeSelector.value;
+})
+
+color.addEventListener("change", (event)=>{
+  r.style.setProperty('--colorPick', event.target.value);
+  console.log(r.style.getPropertyValue("--colorPick"));
+});
+
+
+createTable(16);
